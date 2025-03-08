ccc255250dd2829b03797ab562b1a96785d67ae7
HermanDBustosZ <hermanbustos@unicomfacauca.edu.co>
2025-03-08 16:08:27 -0500

    Funciones 1 y 2

--- ejercicios.py
+++ ejercicios.py
@@ -7,3 +7,22 @@ def lista_aleatoria():
         
 
 lista_aleatoria()
+
+
+def abecedario(n):
+  abeced = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
+  listabc = abeced.split(',')
+  for i in range(27):
+    if (i % n != 0):
+      print(listabc[i-1])
+
+
+def vocales(frase):
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  numvowels =[0,0,0,0,0]
+  cuentavocal = 0
+  for i in range(5):
+    numvowels[i] = frase.count(vowels[i])
+    #print('La frase tiene',numvowels[i],"veces la vocal '",vowels[i],"'.")
+    cuentavocal = cuentavocal + numvowels[i]
+  print(cuentavocal)
