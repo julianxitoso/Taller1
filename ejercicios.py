88b220fb25dd3aca934ca633e1cec3f1981ded84
HermanDBustosZ <hermanbustos@unicomfacauca.edu.co>
2025-03-08 16:36:21 -0500

    Funciones 1 y 2

--- ejercicios.py
+++ ejercicios.py
@@ -1,22 +1,14 @@
-import random
-
-def lista_aleatoria():
-    numeros = [random.randint(1, 10) for _ in range(10)]
-    for num in numeros:
-        print(f"Número: {num}, Cuadrado: {num**2}, Cubo: {num**3}")
-        
-
-lista_aleatoria()
-
-
+# Ejercicio 1
 def abecedario(n):
-  abeced = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
-  listabc = abeced.split(',')
+  abeced = 'abcdefghijklmnopqrstuvwxyz'
+  listabc = list(abeced)
   for i in range(27):
     if (i % n != 0):
       print(listabc[i-1])
 
+abecedario(3)
 
+# Ejercicio 2
 def vocales(frase):
   vowels = ['a', 'e', 'i', 'o', 'u']
   numvowels =[0,0,0,0,0]
@@ -26,3 +18,16 @@ def vocales(frase):
     #print('La frase tiene',numvowels[i],"veces la vocal '",vowels[i],"'.")
     cuentavocal = cuentavocal + numvowels[i]
   print(cuentavocal)
+
+vocales("otorrinoralingologo")
+
+import random
+def lista_aleatoria():
+    numeros = [random.randint(1, 10) for _ in range(10)]
+    for num in numeros:
+        print(f"Número: {num}, Cuadrado: {num**2}, Cubo: {num**3}")
+        
+
+lista_aleatoria()
+
+
